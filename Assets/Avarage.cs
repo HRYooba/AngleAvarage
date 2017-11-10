05fa30c98e4e28753d07f916bec07d470a79685c
大場洋哉 <HRYooba@gmail.com>
2017-11-10 21:37:05 +0900

    update system use Quaternion

--- Assets/Avarage.cs
+++ Assets/Avarage.cs
@@ -35,16 +35,24 @@ public class Avarage : MonoBehaviour
         display1 = obj1.transform.eulerAngles;
         display2 = obj2.transform.eulerAngles;
 
-        float x = BlendAngle(display1.x, display2.x);
-        float y = BlendAngle(display1.y, display2.y);
-        float z = BlendAngle(display1.z, display2.z);
-        blendAngle = new Vector3(x, y, z);
-        blendObj.transform.eulerAngles = blendAngle;
+        // float x = BlendAngle(display1.x, display2.x);
+        // float y = BlendAngle(display1.y, display2.y);
+        // float z = BlendAngle(display1.z, display2.z);
+        // blendAngle = new Vector3(x, y, z);
+        // blendAngle = display1 * inRate + display2 * blendRate;
+        // blendObj.transform.eulerAngles = blendAngle;
+
+        float x = obj1.transform.rotation.x * inRate + obj2.transform.rotation.x * blendRate;
+        float y = obj1.transform.rotation.y * inRate + obj2.transform.rotation.y * blendRate;
+        float z = obj1.transform.rotation.z * inRate + obj2.transform.rotation.z * blendRate;
+        float w = obj1.transform.rotation.w * inRate + obj2.transform.rotation.w * blendRate;
+        blendObj.transform.rotation = new Quaternion(x, y, z, w);
 
         display3 = blendObj.transform.eulerAngles;
     }
 
-    float BlendAngle(float angle1, float angle2) {
+    float BlendAngle(float angle1, float angle2)
+    {
         float sin1 = Mathf.Sin(angle1 / 180.0f * Mathf.PI);
         float cos1 = Mathf.Cos(angle1 / 180.0f * Mathf.PI);
         float sin2 = Mathf.Sin(angle2 / 180.0f * Mathf.PI);
